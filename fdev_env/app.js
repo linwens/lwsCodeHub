1acaa092238cf37e2b500961104e5e6267f5a3ed
linwens <lws200841001@gmail.com>
2017-08-29 18:01:55 +0800

    搭建前端本地开发环境demo

---
+++ fdev_env/app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var lessMiddleware = require('less-middleware');
+
+var index = require('./routes/index');//处理页面路由
+var users = require('./routes/users');//处理请求url
+
+var app = express();
+
+// view engine setup  ///修改ejs渲染方式，express先解析HTML文件，同时能够渲染ejs的代码片段
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(lessMiddleware(path.join(__dirname, 'public')));//less解析中间件
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
