c8be15760d8b1a8e9aada351e11a253f2cafcae4
linwens <lws200841001@gmail.com>
2017-09-04 14:48:12 +0800

    增加通配符判断路由

--- fdev_env/routes/index.js
+++ fdev_env/routes/index.js
@@ -5,8 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/activity/nationalDay.html', function(req, res, next) {
-  res.render('activity/nationalDay', { title: 'Express' });
-});
+//使用通配符，再根据url去查找文件目录
+router.get('*',function(req,res,next){
+	res.render(req.path.slice(1,req.path.length-5), { title: 'Express' });
+})
 
 module.exports = router;
