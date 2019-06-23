9e73cc0eeeac2d8a55f471fc3331b6f85d1a56da
linwens <lws200841001@gmail.com>
2019-06-23 23:37:38 +0800

    5.5.4

--- js_TDG/5/code.js
+++ js_TDG/5/code.js
@@ -7,3 +7,38 @@
   cx = Math.cos(x);
   console.log("cos(π) = " + cx)
 }
+// 空语句
+var a = []
+for (i = 0; i < a.length; a[i++] = 0 ); /* empty */
+
+/**
+ * 5.5.1
+ */
+var count = 0;
+while (count < 10) {
+  console.log(count)
+  count++;
+}
+
+/**
+ * 5.5.2
+ */
+function printArray(a) {
+  var len = a.length, i = 0;
+  if (len == 0) {
+    console.log("Empty Array")
+  } else {
+    do {
+      console.log(a[i]);
+    } while (++i < len);
+  }
+}
+printArray('ab')
+
+/**
+ * 5.5.3
+ */
+function tail(o) {
+  for(; o.text; o = o.next) /* empty */
+  return o;
+}
