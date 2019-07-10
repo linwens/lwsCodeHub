97b61052d36310e53980c3aae5210adf9087674f
linwens <lws200841001@gmail.com>
2019-07-11 00:00:09 +0800

    9.6.1

--- js_TDG/9/code.js
+++ js_TDG/9/code.js
@@ -177,3 +177,89 @@ function classof(o) {
 /**
  * 9.5.4
  */
+function quacks(o /*, ... */) {
+  for (var i = 1; i < arguments.length; i++) {
+    var arg = arguments[i];
+    switch (typeof arg) {
+      case 'string': 
+        if (typeof o[arg] !== "function") return false;
+        continue;
+      case 'function':
+        arg = arg.prototype;
+      case 'object':
+        for (var m in arg) {
+          if (typeof arg[m] !== "function") continue;
+          if (typeof o[m] !== "function") return false;
+        }
+    }
+  }
+  return true
+}
+
+/**
+ * 9.6.1 集合类
+ */
+function Set() {
+  this.values = {};
+  this.n = 0;
+  this.add.apply(this, arguments);
+}
+
+Set.prototype.add = function() {
+  for (var i = 0; i < arguments.length; i++) {
+    var val = arguments[i];
+    var str = Set.v2s(val);
+    if (!this.values.hasOwnProperty(str)) {
+      this.values[str] = val;
+      this.n++;
+    }
+  }
+  return this;
+}
+Set.prototype.remove = function() {
+  for (var i = 0; i < arguments.length; i++) {
+    var str = Set._v2s(arguments[i]);
+    if (this.values.hasOwnProperty(str)) {
+      delete this.values[str];
+      this.n--;
+    }
+  }
+  return this;
+}
+Set.prototype.contains = function(value) {
+  return this.values.hasOwnProperty(Set._v2s(value));
+}
+Set.prototype.size = function() {
+  return this.n;
+}
+Set.prototype.foreach = function(f, context) {
+  for (var s in this.values) {
+    if (this.values.hasOwnProperty(s)) {
+      f.call(context, this.values[s])
+    }
+  }
+}
+Set._v2s = function(val) {
+  switch (val) {
+    case undefined: return 'u';
+    case null:      return 'n';
+    case true:      return 't';
+    case false:     return 'f';
+    default: switch (typeof val) {
+      case 'number': return '#' + val;
+      case 'string': return '"' + val;
+      default: return '@' + objectId(val)
+    }
+  }
+  function objectId(o) {
+    var prop = "|**objectid**|";
+    if (!o.hasOwnProperty(prop)) {
+      o[prop] = Set._v2s.next++;
+    }
+    return o[prop];
+  }
+}
+Set._v2s.next = 100; // 设置初始id值
+/**
+ * 9.6.2
+ */
