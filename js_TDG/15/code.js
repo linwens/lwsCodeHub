4d28035717030347a1ce89d1cad1b734e64514d0
linwens_xdf <linwenshu@xdf.cn>
2019-08-30 17:49:17 +0800

    抄权威指南，到15.2.3

--- js_TDG/15/code.js
+++ js_TDG/15/code.js
@@ -1,2 +1,20 @@
-
+/**
+ * 15.2.1
+ */
  
+function getElements(/*ids...*/) {
+  var elements = {};
+  for(var i = 0; i < arguments.length; i++) {
+    var id = arguments[i];
+    var elt = document.getElementById(id);
+    if (elt == null) {
+      throw new Error("No element width id: " + id);
+    }
+    elements[id] = elt;
+  }
+  return elements;
+}
+
+/**
+ * 15.2.3
+ */
