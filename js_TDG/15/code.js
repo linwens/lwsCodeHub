9f77faeead93dc2245d2ca647fea77d27b0abe42
linwens <lws200841001@gmail.com>
2019-09-06 08:05:59 +0800

    到15.8.2

--- js_TDG/15/code.js
+++ js_TDG/15/code.js
@@ -417,3 +417,52 @@ window.onload = function() {
     toc.appendChild(entry);
   }
 }
+
+/**
+ * 15.8.1
+ */
+function getScrollOffset(w) {
+
+  w = w || window;
+  if (w.pageXOffset != null) {
+    return {x: w.pageXOffset, y: w.pageYOffset}
+  }
+
+  var d = w.document;
+  if (document.compatMode == "CSS1Compat") {
+    return {
+      x: d.documentElement.scrollLeft,
+      y: d.documentElement.scrollTop
+    }
+  }
+
+  return {
+    x: d.body.scrollLeft,
+    y: d.body.scrollTop
+  }
+}
+
+function getViewportSize(w) {
+  
+  w = w || window;
+  if (w.innerWidth != null) {
+    return {w: w.innerWidth, h: w.innerHeight}
+  }
+
+  var d = w.document;
+  if (document.compatMode == "CSS1Compat") {
+    return {
+      w: d.documentElement.clientWidth,
+      h: d.documentElement.clientHeight
+    }
+  }
+
+  return {
+    w: d.body.clientWidth,
+    h: d.body.clientHeight
+  }
+}
+
+/**
+ * 15.8.2
+ */
