eccda8bc90bb26ef0f712e294a80785563e3c881
linwens_xdf <linwenshu@xdf.cn>
2019-09-10 18:07:47 +0800

    到13.3.2

--- js_TDG/13/code.js
+++ js_TDG/13/code.js
@@ -1,3 +1,26 @@
 /**
- * 13.1.1
+ * 13.3.1
  */
+function loadasync(url) {
+  var head = document.getElementsByTagName("head")[0];
+  var s = document.createElement("script");
+  s.src = url;
+  head.appendChild(s);
+}
+
+/**
+ * 13.3.2
+ */
+function onLoad(f) {
+  if (onLoad.loaded) {
+    window.setTimeout(f, 0);
+  } else if (window.addEventListener) {
+    window.addEventListener("load", f, false);
+  } else if (window.attachEvent) {
+    window.attachEvent("onload", f)
+  }
+}
+onLoad.loaded = false;
+onLoad(function() {
+  onLoad.loaded = true;
+})
