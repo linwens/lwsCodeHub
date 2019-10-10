b7fb8ad69bca5230a0c5827b98107e90c2dd284c
linwens_xdf <linwenshu@xdf.cn>
2019-10-10 10:43:51 +0800

    完成第十章，红黑树还没理清

--- js_algorithms/10/redblacktree.js
+++ js_algorithms/10/redblacktree.js
@@ -2,6 +2,40 @@
  * 10.6.2 红黑树
  */
 
+class Node {
+  constructor(key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const Compare = {
+  LESS_THAN: -1,
+  BIGGER_THAN: 1,
+  EQUALS: 0
+};
+
+function defaultCompare(a, b) {
+  if (a === b) {
+    return Compare.EQUALS;
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+
+class RedBlackNode extends Node {
+  constructor(key) {
+    super(key);
+    this.key = key;
+    this.color = Colors.RED;
+    this.parent = null;
+  }
+
+  isRed() {
+    return this.color === Colors.RED;
+  }
+}
+
 class RedBlackTree extends BinarySearchTree {
   constructor(compareFn = defaultCompare) {
     super(compareFn);
@@ -19,5 +53,107 @@ class RedBlackTree extends BinarySearchTree {
     }
   }
 
-  
+  insertNode(node, key) {
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      if (node.left == null) {
+        node.left = new RedBlackNode(key);
+        node.left.parent = node;
+        return node.left;
+      } else {
+        return this.insertNode(node.left, key);
+      }
+    } else if (node.right == null) {
+      node.right = new RedBlackNode(key);
+      node.right.parent = node;
+      return node.right;
+    } else {
+      return this.insertNode(node.right, key);
+    }
+  }
+
+  rotationLL(node) {
+    const tmp = node.left;
+    node.left = tmp.right;
+    if (tmp.right && tmp.right.key) {
+      tmp.right.parent = node;
+    }
+    tmp.parent = node.parent;
+    if (!node.parent) {
+      this.root = tmp;
+    } else {
+      if (node === node.parent.left) {
+        node.parent.left = tmp;
+      } else {
+        node.parent.right = tmp;
+      }
+      tmp.right = node;
+      node.parent = tmp;
+    }
+  }
+
+  rotationRR(node) {
+    const tmp = node.right;
+    node.right = tmp.left;
+    if (tmp.left && tmp.left.key) {
+      tmp.left.parent = node;
+    }
+    tmp.parent = node.parent;
+    if (!node.parent) {
+      this.root = tmp;
+    } else {
+      if (node === node.parent.left) {
+        node.parent.left = tmp;
+      } else {
+        node.parent.right = tmp;
+      }
+    }
+    tmp.left = node;
+    node.parent = tmp;
+  }
+
+  fixTreeProperties(node) {
+    while (node && node.parent && node.parent.color.isRed() && node.color !== Colors.BLACK) {
+      let parent = noode.parent;
+      const grandParent = parent.parent;
+      
+      if (grandParent && grandParent.left === parent) {
+        const uncle = grandParent.right;
+        if (uncle && uncle.color === Colors.RED) {
+          grandParent.color = Colors.RED;
+          parent.color = Colors.BLACK;
+          uncle.color = Colors.BLACK;
+          node = grandParent;
+        } else {
+          if (node === parent.right) {
+            this.rotationRR(parent);
+            node = parent;
+            parent = node.parent;
+          }
+          this.rotationLL(grandParent);
+          parent.color = Colors.BLACK;
+          grandParent.color = Colors.RED;
+          node.parent;
+        }
+      } else {
+        const uncle = grandParent.left;
+        if (uncle && uncle.color === Colors.RED) {
+          grandParent.color = Colors.RED;
+          parent.color = Colors.BLACK;
+          uncle.color = Colors.BLACK;
+          node = grandParent;
+        } else {
+          if (node === parent.left) {
+            this.rotationLL(parent);
+            node = parent;
+            parent = node.parent;
+          }
+          this.rotationRR(grandParent);
+          parent.color = Colors.BLACK;
+          grandParent.color = Colors.RED;
+          node = parent;
+        }
+      }
+    }
+    this.root.color = Colors.BLACK;
+  }
 }
