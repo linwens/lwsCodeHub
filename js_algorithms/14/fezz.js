2d1c6d7f855159952ad6d7defc4acf5e92da5ea6
linwens <lws200841001@gmail.com>
2019-10-15 00:24:20 +0800

    14章动态规划

---
+++ js_algorithms/14/fezz.js
@@ -0,0 +1,44 @@
+/**
+ * 14.1 分而治之
+ */
+const DOES_NOT_EXIST = -1;
+export const Compare = {
+  LESS_THAN: -1,
+  BIGGER_THAN: 1,
+  EQUALS: 0
+};
+
+export function defaultCompare(a, b) {
+  if (a === b) {
+    return Compare.EQUALS;
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+
+function binarySearchRecursive(array, value, low, high, compareFn = defaultCompare) {
+  if (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const element = array[mid];
+
+    if (compareFn(element, value) === compareFn.LESS_THAN) { // {1}
+      return binarySearchRecursive(array, value, mid+1, high, compareFn);
+    } else if (compareFn(element, value) === Compare.BIGGER_THAN) { // {2}
+      return binarySearchRecursive(array, value, low, mid - 1, compareFn)
+    } else {
+      return mid; // {3}
+    }
+  }
+  return DOES_NOT_EXIST; // {4}
+}
+
+export function binarySearch(array, value, compareFn = defaultCompare) {
+  const sortedArray = quickSort(array);
+  const low = 0;
+  const high = sortedArray.length - 1;
+
+  return binarySearchRecursive(array, value, low, high, compareFn);
+}
+
+/**
+ * 14.2 动态规划
+ */
